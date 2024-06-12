bc322723334982acacf6c8c742719e31e5cf31b4
Paulo Barros <paulo.barros@lambda3.com.br>
2024-06-12 17:05:44 -0300

    feat: create api-gateway

---
+++ api-gateway/src/app.controller.ts
@@ -0,0 +1,13 @@
+import {Body, Controller, Get, Post} from '@nestjs/common'
+import {AppService} from './app.service'
+import {CreateOrderRequest} from './create-order-request.dto'
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post()
+  createOrder(@Body() createOrderRequest: CreateOrderRequest) {
+    this.appService.createOrder(createOrderRequest)
+  }
+}
