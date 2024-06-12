b66f11a02e3b4ee477696ad0f342dab0bf3a92d7
Paulo Barros <paulo.barros@lambda3.com.br>
2024-06-12 17:07:06 -0300

    feat: create auth microservice

---
+++ billing/src/app.controller.ts
@@ -0,0 +1,21 @@
+import {Controller, Get, Inject, OnModuleInit} from '@nestjs/common';
+import {ClientKafka, EventPattern} from '@nestjs/microservices';
+import {AppService} from './app.service';
+
+@Controller()
+export class AppController implements OnModuleInit {
+  constructor(
+    private readonly appService: AppService,
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientKafka,
+  ) {}
+
+  @EventPattern('order_created')
+  handleOrderCreated(data: any) {
+    console.log(data);
+    this.appService.handleOrderCreated(data);
+  }
+
+  onModuleInit() {
+    this.authClient.subscribeToResponseOf('get_user');
+  }
+}
---
+++ billing/src/app.service.ts
@@ -0,0 +1,22 @@
+import {Inject, Injectable} from '@nestjs/common';
+import {ClientKafka} from '@nestjs/microservices';
+import {GetUserRequest} from './get-user-request.dto';
+import {OrderCreatedEvent} from './order-created.event';
+
+@Injectable()
+export class AppService {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientKafka,
+  ) {}
+
+  handleOrderCreated(orderCreatedEvent: OrderCreatedEvent) {
+    console.log('Billing service received order created event', orderCreatedEvent);
+    this.authClient
+      .send('get_user', new GetUserRequest(orderCreatedEvent.userId))
+      .subscribe((user) => {
+        console.log(
+          `Billing user with stripe ID ${user.stripeUserId} a price of $${orderCreatedEvent.price}...`,
+        );
+      });
+  }
+}
